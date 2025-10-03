b79c654c16c0ee8ad68216f0653dff97cc0cf403
Kei Takeuchi <takeuchi@landbridge.co.jp>
2025-10-03 12:02:46 +0900

    Fix syntax error in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
               >
                 {item.label}
               </Link>
-            ))
+            ))}
           </nav>
 
           {/* モバイルメニューボタン */}
